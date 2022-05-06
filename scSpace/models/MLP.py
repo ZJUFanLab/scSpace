cd18850c866604f9512f2fdf98c1549663b98fc7
Qian Jingyang <qianjingyang@zju.edu.cn>
2022-05-06 14:58:23 +0800

    Update MLP.py

--- scSpace/models/MLP.py
+++ scSpace/models/MLP.py
@@ -1,27 +1,6 @@
 from torch import nn
 
 
-class MLPEncoder(nn.Module):
-    """MLPEncoder encoder model for scSpace."""
-
-    def __init__(self, input_size):
-        """Init MLP encoder."""
-        super(MLPEncoder, self).__init__()
-        self.restored = False
-        common_size = 2
-        self.linear = nn.Sequential(
-            nn.Linear(input_size, input_size // 2),
-            nn.ReLU(inplace=True),
-            nn.Linear(input_size // 2, input_size // 4),
-            nn.ReLU(inplace=True),
-            nn.Linear(input_size // 4, common_size)
-        )
-
-    def forward(self, x):
-        out = self.linear(x)
-        return out
-
-
 class sample_MLPEncoder(nn.Module):
     """sample MLPEncoder encoder model for scSpace."""
 
